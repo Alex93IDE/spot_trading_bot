a0c213c40dd8e219e7dfb978c3922455b2a50916
Alex93IDE <adsf.arduino@gmail.com>
2022-06-21 14:14:46 -0400

    routes for bot aplication

--- Routes/Controllers/app.js
+++ Routes/Controllers/app.js
@@ -1,5 +1,6 @@
 const Storage = require('node-storage')
-const { bot_struct } = require('../../utils/bot')
+const { bot_struct, getPriceMarket, getBalance } = require('../../utils/bot')
+const { log, logColor, colors } = require('../../utils/logger')
 
 exports.home = function home(req, res) {
     let store = new Storage(`./data/${bot_struct.MARKET}.json`)
@@ -15,4 +16,51 @@ exports.restart = function restart(req, res) {
         process.exit()
     }, 1000);
     res.json("done")
+}
+
+exports.process_bot = async function a(req, res) {
+    let data = req.query;
+    const words = data.symbol.split('-')
+    let symbol_1 = words[0]
+    let symbol_2 = words[1]
+    
+    switch (data.status) {
+        case 'start':
+            bot_struct.MARKET1 = symbol_1
+            bot_struct.MARKET2 = symbol_2
+            bot_struct.MARKET = data.symbol
+            bot_struct.base_quote = parseInt(data.fund)
+            let store = new Storage(`./data/${bot_struct.MARKET}.json`)
+
+            store.put('start_price', await getPriceMarket(bot_struct.MARKET))
+            store.put('orders', []);
+            store.put('profits', 0);
+            store.put('percent', 0);
+            store.put(`${symbol_1}_balance`, await getBalance(symbol_1));
+            store.put(`${symbol_2}_balance`, await getBalance(symbol_2));
+            store.put(`initial_${symbol_1}_balance`, store.get(`${symbol_1}_balance`));
+            store.put(`initial_${symbol_2}_balance`, store.get(`${symbol_2}_balance`));
+
+            logColor(colors.green, 'BOT ENCENDIDO')
+            bot_struct.start_bot_trading = 1;
+            res.json('BOT ENCENDIDO START')
+            break;
+        case 'continue':
+            bot_struct.MARKET1 = symbol_1
+            bot_struct.MARKET2 = symbol_2
+            bot_struct.MARKET = data.symbol
+            bot_struct.base_quote = parseInt(data.fund)
+
+            logColor(colors.green, 'BOT ENCENDIDO')
+            bot_struct.start_bot_trading = 1;
+            res.json('BOT ENCENDIDO CONTINUE')
+            break;
+        case 'stop':
+            bot_struct.start_bot_trading = 0;
+            res.json('BOT DETENIDO')
+            break;
+        default:
+            res.json('status fall')
+            break;
+    }
 }
--- Routes/routes.js
+++ Routes/routes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const ruta = Router();
 
-const { home, restart } = require('./Controllers/app')
+const { home, restart, process_bot } = require('./Controllers/app')
 
 ruta.get('/', home)
 ruta.get('/restart', restart)
+ruta.get('/bot', process_bot)
 
 module.exports = ruta;
